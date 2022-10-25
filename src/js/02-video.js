61b6a4da09ed60a07220766bd67560f5598ef1d3
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2022-10-25 23:39:08 +0300

    added vimeo-player npm and some code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
 
+var throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const getTimeUpdate = ({ seconds }) => {
+  console.log('seconds ', Math.floor(seconds));
+};
+
+player.on('timeupdate', throttle(getTimeUpdate, 1000, { trailing: false }));
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
