1eed3d902ca87362f13642349ea7db2b0554cd8e
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2022-10-26 23:09:27 +0300

    begin task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,6 @@
+import * as throttle from 'lodash.throttle';
 
+const getEl = elemetn => document.querySelector(elemetn);
+const form = getEl('.feedback-form');
+console.log('form ', form);
+console.log('form ', form.elements[0]);
