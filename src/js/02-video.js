4b0a0f5812ecc7ec77ff971c531fe3bed9e97bb1
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2022-10-26 22:05:22 +0300

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,33 +1,49 @@
 import Player from '@vimeo/player';
 
-var throttle = require('lodash.throttle');
+let throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const getTimeUpdate = ({ seconds }) => {
-  console.log('seconds ', Math.floor(seconds));
+const getCurrentTime = ({ seconds }) => {
+  saveCurrentTimeToLS(Math.floor(seconds));
 };
 
-player.on('timeupdate', throttle(getTimeUpdate, 1000, { trailing: false }));
+player.on('timeupdate', throttle(getCurrentTime, 1000, { trailing: false }));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
+const currentTime = getCurrentTimeFromLS();
 player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
+  .setCurrentTime(currentTime)
+  .then()
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
+        console.log('Error: ', error.message);
         // the time was less than 0 or greater than the video’s duration
         break;
-
       default:
         // some other error occurred
         break;
     }
   });
+
+function saveCurrentTimeToLS(time) {
+  try {
+    localStorage.setItem('videoplayer-current-time', time);
+  } catch (error) {
+    console.log('Error: ', error.message);
+  }
+}
+
+function getCurrentTimeFromLS() {
+  try {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    if (!Number(currentTime) || currentTime < 0) {
+      console.log('Error: ', 'Wrong local data!');
+      return 0;
+    }
+    return currentTime;
+  } catch (error) {
+    console.log('Error: ', error.message);
+  }
+}
