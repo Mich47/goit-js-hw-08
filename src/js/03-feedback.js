563fa0b25228be3831cd50ab677ebca9beefbb5f
Mich47 <mmitya889@gmail.com>
2022-10-27 17:53:43 +0300

    add code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,36 @@
 import * as throttle from 'lodash.throttle';
 
-// const getEl = elemetn => document.querySelector(elemetn);
-// const form = getEl('.feedback-form');
-// const getEl = elemetn => document.querySelector(elemetn);
-const form = document.querySelector('.feedback-form');
+const getEl = elemetn => document.querySelector(elemetn);
+const formRef = getEl('.feedback-form');
+const ref = {
+  email: getEl('input[name="email"]'),
+  message: getEl('textarea[name="message"]'),
+};
 
-form.addEventListener('submit', event => {
+console.log(ref.form);
+console.log(ref.email);
+console.log(ref.message);
+
+const feedbackFormData = { email: '', message: '' };
+
+formRef.addEventListener('input', event => {
+  console.log(event.target);
+  if ('INPUT' === event.target.nodeName) {
+    feedbackFormData.email = event.target.value;
+  }
+  if ('TEXTAREA' === event.target.nodeName) {
+    feedbackFormData.message = event.target.value;
+  }
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormData));
+});
+
+formRef.addEventListener('submit', event => {
   console.log('object');
   event.preventDefault();
   console.log(event);
   const {
     elements: { email, message },
-  } = event.curentTarget;
+  } = event.currentTarget;
   console.log('1', email);
   console.log('2', message);
 });
-// console.log('form ', form);
-console.log('form ', form.elements[0]);
