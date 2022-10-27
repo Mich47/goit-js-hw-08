bb452011cbbbf375e27f8f281da302aad1d3a41f
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2022-10-27 11:11:54 +0300

    add code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,19 @@
 import * as throttle from 'lodash.throttle';
 
-const getEl = elemetn => document.querySelector(elemetn);
-const form = getEl('.feedback-form');
-console.log('form ', form);
+// const getEl = elemetn => document.querySelector(elemetn);
+// const form = getEl('.feedback-form');
+// const getEl = elemetn => document.querySelector(elemetn);
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', event => {
+  console.log('object');
+  event.preventDefault();
+  console.log(event);
+  const {
+    elements: { email, message },
+  } = event.curentTarget;
+  console.log('1', email);
+  console.log('2', message);
+});
+// console.log('form ', form);
 console.log('form ', form.elements[0]);
